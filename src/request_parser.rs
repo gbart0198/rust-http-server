dedaf739d067a00ee8f5a509b53892ecc29ee6b2
gbart0198 <gbart0198@gmail.com>
2024-11-23 19:08:15 -0500

    revamped read to use BufReader

--- src/request_parser.rs
+++ src/request_parser.rs
@@ -4,7 +4,8 @@
 // 4. Handle request body (if present)
 //
 
-use std::{collections::HashMap, io::Read, net::TcpStream, str};
+use std::io::{BufRead, BufReader, Read};
+use std::{collections::HashMap, net::TcpStream, str};
 
 #[derive(Debug)]
 pub struct HttpRequest {
@@ -36,46 +37,53 @@ impl HttpRequest {
 pub struct HttpRequestParser;
 
 impl HttpRequestParser {
-    pub fn handle_http_request(mut stream: &TcpStream) -> HttpRequest {
-        let mut buf = [0; 4096];
-        let num_read = &stream.read(&mut buf).unwrap();
-        let request_info: Vec<&str> = str::from_utf8(&buf[..*num_read])
-            .unwrap()
-            .trim()
-            .split(" ")
-            .collect();
-        let request_type = request_info[0].to_string();
-        let route = request_info[1].to_string();
-        let request_version = request_info[2].to_string();
-
-        let mut headers = HashMap::new();
+    pub fn handle_http_request(stream: &TcpStream) -> HttpRequest {
+        let mut reader = BufReader::new(stream);
         let mut content_length = 0;
+        let mut headers = HashMap::new();
+        let mut line = String::new();
+
+        reader.read_line(&mut line).unwrap();
+        let line_vec: Vec<&str> = line.trim().split(" ").collect();
+        println!("{:?}", line_vec);
+
+        assert_eq!(line_vec.len(), 3);
+
+        let request_type = line_vec[0].to_string();
+        let route = line_vec[1].to_string();
+        let request_version = line_vec[2].to_string();
+        let mut content = String::new();
+
         loop {
-            let num_read = &stream.read(&mut buf).unwrap();
-            let header_line = str::from_utf8(&buf[..*num_read]).unwrap();
-            if header_line != "\r\n" {
-                let header_info: Vec<_> = header_line.split(":").collect();
-                let key = header_info[0].trim();
-                let val = header_info[1].trim();
-                if header_line.contains("Content-Length") {
-                    content_length = header_info[header_info.len() - 1]
-                        .trim()
-                        .parse::<usize>()
-                        .unwrap();
+            let mut header = String::new();
+            reader.read_line(&mut header).unwrap();
+            match header.find(":") {
+                Some(key_break) => {
+                    let key = &header[0..key_break];
+                    let val = &header[key_break..header.len()];
+                    if header == "Content-Length" {
+                        content_length = val.parse().unwrap();
+                    }
+
+                    headers.insert(key.to_string(), val.trim().to_string());
                 }
-                headers.insert(key.to_string(), val.to_string());
-            } else {
-                break;
+                None => {
+                    if content_length == 0 {
+                        break;
+                    }
+                    // empty line between header and content
+                    let mut bytes_read = 0;
+                    while bytes_read < content_length {
+                        bytes_read += reader.read_line(&mut header).unwrap();
+                        content.push_str(&header);
+                    }
+                }
             }
         }
-        let mut content = String::new();
-        if content_length > 0 {
-            let _ = &stream.read_exact(&mut buf[..content_length + 1]).unwrap();
-            content = str::from_utf8(&buf[..content_length + 1])
-                .unwrap()
-                .to_owned();
-        }
 
-        return HttpRequest::new(request_type, route, request_version, headers, content);
+        let req = HttpRequest::new(request_type, route, request_version, headers, content);
+        println!("{:?}", req);
+
+        req
     }
 }
